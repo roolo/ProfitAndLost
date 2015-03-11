611d0933b5d3f171a51ef402a3a827ae6b07a70c
Mailo Svetel <development@rooland.cz>
2015-03-11 15:03:49 +0100

    [user][devise] Initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # @param [String] app_label
+  def bootstrap_label_class app_label
+    { 'notice' => 'success',
+      'alert' => 'danger'
+    }[app_label] || app_label
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'root/index'
 
   root to: 'root#index'
